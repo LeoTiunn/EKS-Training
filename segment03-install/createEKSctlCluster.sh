7864a74425d6053ff813e70a42f0255d04189767
Leo Chang <chihshun@live.com>
2021-08-11 00:42:09 -0700

    terraform state file

--- segment03-install/createEKSctlCluster.sh
+++ segment03-install/createEKSctlCluster.sh
@@ -2,10 +2,9 @@
 set -x
 time eksctl create cluster \
 --name dec31 \
---version 1.18 \
---region us-west-2 \
+--version 1.21 \
 --nodegroup-name standard-workers \
---node-type t3.medium \
+--node-type t2.micro \
 --nodes 2 \
 --nodes-min 1 \
 --nodes-max 4 \
